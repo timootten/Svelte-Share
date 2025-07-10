01d7d6963754f72ce36478735517724dfb5fec52
Timo Otten <timootten@icloud.com>
2025-07-10 15:38:59 +0200

    Updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { PUBLIC_PEER_PREFIX } from '$env/static/public';
+	import { onMount } from 'svelte';
+	import { default as Peer } from 'peerjs';
+	import { generateRandomString } from '$lib/utils';
+
+	let loading = true;
+	let error = true;
+	let errorMessage = '';
+	let id = '';
+	let peerId = '';
+
+	onMount(() => {
+		id = generateRandomString();
+		peerId = (PUBLIC_PEER_PREFIX ?? 'SHARE_FJEIDKD_') + '_' + id;
+
+		const peer = new Peer(peerId);
+
+		peer.on('open', (id) => {
+			console.log('Peer ID:', id);
+			loading = false;
+		});
+
+		peer.on('error', (err) => {
+			console.error('Peer error:', err);
+			error = true;
+			errorMessage = err.message || 'An unknown error occurred';
+		});
+	});
+</script>
+
+{id}
+{peerId}
